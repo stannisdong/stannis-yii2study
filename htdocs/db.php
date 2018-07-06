e9333c9e760ec75ec10a8735befe5538f34a31e7
stannisdong <stannisdong@augmentum.com.cn>
2018-07-06 15:30:22 +0800

    feat(apache): remove

--- htdocs/db.php
+++
@@ -1,8 +0,0 @@
-<?php
-	$mysqli = mysqli_connect("localhost","root","111111","test");
-	if (!$mysqli) {
-		echo 'Database connect failed!';
-	} else {
-		echo 'Database connect successfully!';
-	}
-?>
